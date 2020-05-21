5a9304af7e41fadcfaf3f736b414d87608f73f09
Henrich Lauko <xlauko@mail.muni.cz>
2020-05-21 09:47:20 +0200

    lib: add license notes

--- include/lart/choose.hpp
+++ include/lart/choose.hpp
@@ -1,1 +1,5 @@
+// Copyright (C) 2020 Henrich Lauko <xlauko@mail.muni.cz>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level directory of this distribution.
+
 #pragma once
--- include/lart/taint.hpp
+++ include/lart/taint.hpp
@@ -1,3 +1,7 @@
+// Copyright (C) 2020 Henrich Lauko <xlauko@mail.muni.cz>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level directory of this distribution.
+
 #pragma once
 
 #include <sanitizer/dfsan_interface.h>
